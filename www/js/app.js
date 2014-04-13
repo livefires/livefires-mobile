fe6d549b47b058494328a941c1251f2f3c6f5f3c
Javier Rubio <hola@javirubio.net>
2014-04-13 18:11:35 +0200

    remove template files

--- www/js/app.js
+++ www/js/app.js
@@ -32,44 +32,8 @@ angular.module('starter', ['ionic', 'starter.controllers', "leaflet-directive"])
           templateUrl: "templates/map.html"
         }
       }
-    })
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
     });
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/map');
 });
